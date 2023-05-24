2dc313e387f8227d696386c47380aee2ba7fe0ab
fiskinafiulafida <fiskinafiul@gmail.com>
2023-05-24 18:34:39 +0700

    first commit

---
+++ resources/views/Login/main.blade.php
@@ -0,0 +1,37 @@
+@extends('Layouts/LogReg')
+@section('title', 'Sign In - Sistem Informasi Moora')
+@section('section')
+<div class="container">
+    <div class="signin-content">
+        <div class="signin-image">
+            <figure><img src="{{ asset('styleLog/images/signin-image.jpg')}}" alt="sing up image"></figure>
+            <a href="#" class="signup-image-link">Create an account</a>
+        </div>
+
+        <div class="signin-form">
+            <h2 class="form-title">Sign In</h2>
+            <form action="/login" method="POST" class="register-form" id="login-form">
+                @csrf
+                <div class="form-group">
+                    <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
+                    <input type="text" name="name" id="name" placeholder="Your Name" />
+                </div>
+                <div class="form-group">
+                    <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
+                    <input type="password" name="password" id="password" placeholder="Password" />
+                </div>
+                <div class="form-group form-button">
+                    <input type="submit" class="form-submit" />
+                </div>
+            </form>
+            <div class="social-login">
+                <ul class="socials">
+                    <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
+                    <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
+                    <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/main_home.blade.php
@@ -0,0 +1,111 @@
+@extends('Layouts/Admin')
+
+@section('title', 'Dashboard Admin')
+
+@section('sidebar')
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/home">
+        <div class="sidebar-brand-icon rotate-n-15">
+            <i class="fas fa-laugh-wink"></i>
+        </div>
+        <div class="sidebar-brand-text mx-3">Admin <sup>Moora</sup></div>
+    </a>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider my-0">
+
+    <!-- Nav Item - Dashboard -->
+    <li class="nav-item">
+        <a class="nav-link" href="/home">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Dashboard</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider">
+
+    <!-- Nav Item - Pages Collapse Menu -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Components</span>
+        </a>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Components</h6>
+                <a class="collapse-item" href="buttons.html">Alternatif</a>
+                <a class="collapse-item" href="cards.html">Kriteria</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Charts -->
+    <li class="nav-item">
+        <a class="nav-link" href="charts.html">
+            <i class="fas fa-fw fa-chart-area"></i>
+            <span>Perhitungan</span></a>
+    </li>
+
+    <!-- Divider -->
+    <hr class="sidebar-divider d-none d-md-block">
+
+    <!-- Sidebar Toggler (Sidebar) -->
+    <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+    </div>
+
+</ul>
+@endsection
+
+@section('topbar')
+<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+    <!-- Sidebar Toggle (Topbar) -->
+    <form class="form-inline">
+        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+        </button>
+    </form>
+
+    <!-- Topbar Search -->
+    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+        <div class="input-group">
+            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+            <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm"></i>
+                </button>
+            </div>
+        </div>
+    </form>
+
+    <!-- Topbar Navbar -->
+    <ul class="navbar-nav ml-auto">
+
+        <!-- Nav Item - User Information -->
+        <li class="nav-item dropdown no-arrow">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                <img class="img-profile rounded-circle" src="{{ asset('style/img/undraw_profile.svg')}}">
+            </a>
+            <!-- Dropdown - User Information -->
+            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="/logout" data-toggle="modal" data-target="#logoutModal">
+                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                    Logout
+                </a>
+            </div>
+        </li>
+
+    </ul>
+
+</nav>
+@endsection
+
+@section('page_heading')
+<h1 class="h3 mb-2 text-gray-800">Tables</h1>
+<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
+    For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+@endsection
