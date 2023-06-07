2757416adf7eec375b4b32899b9e58fb7ce26698
fiskinafiulafida <fiskinafiul@gmail.com>
2023-06-07 19:54:26 +0700

    Edit Kriteria Terbaru

--- resources/views/Kriteria/edit.blade.php
+++ resources/views/Kriteria/edit.blade.php
@@ -114,55 +114,53 @@
 <div>
     <form action="/kriteria/{{ $kriteria->id }}/edit" method="POST">
         @csrf
-        <div class="card-body">
-            <!-- ketika error -->
-            @if ($errors->any())
-            <div class="alert alert-danger">
-                <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                <ul>
-                    @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Nama Kriteria</label>
-                <input type="nama_kriteria" class="form-control" name="nama_kriteria" id="nama_kriteria" value="{{ $kriteria->nama_kriteria }}">
-            </div>
-            <!-- error message untuk Kode -->
-            @error('nama_kriteria')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Bobot</label>
-                <input type="bobot" class="form-control" name="bobot" id="bobot" value="{{ $kriteria->bobot }}">
-            </div>
-            <!-- error message untuk bobot -->
-            @error('bobot')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror
-            <label class="col-sm-2 col-form-label" for="jenis_kriteria">Jenis Kriteria</label>
-            <div class="row mb-3">
-                <div class="col-sm">
-                    <select id="jenis_kriteria" class="form-control" id="jenis_kriteria" name="jenis_kriteria" aria-label="Default select example">
-                        <option value="{{ $kriteria->benefit }}" @selected(old('jenis_kriteria')=='Benefit' )>Benefit</option>
-                        <option value="{{ $kriteria->cost }}" @selected(old('jenis_kriteria')=='Cost' )>Cost</option>
-                    </select>
-                    @error('jenis_kriteria')
-                    <div class="invalid-feedback">
-                        {{ $message }}
-                    </div>
-                    @enderror
+        <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="nama_kriteria">Nama Kriteria</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control @error('nama_kriteria')
+                          is-invalid
+                      @enderror" id="nama_kriteria" name="nama_kriteria" placeholder="10107999" required value="{{ old('nama_kriteria', $kriteria->nama_kriteria) }}" />
+                @error('nama_kriteria')
+                <div class="invalid-feedback">
+                    {{ $message }}
                 </div>
+                @enderror
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Update</button>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="bobot">Bobot (%)</label>
+            <div class="col-sm-10">
+                <input type="number" class="form-control @error('bobot')
+                          is-invalid
+                      @enderror" id="bobot" name="bobot" placeholder="10107999" required value="{{ old('bobot', $kriteria->bobot) }}" />
+                @error('bobot')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for="jenis_kriteria">Jenis Kriteria</label>
+            <div class="col-sm-10">
+                <select id="jenis_kriteria" class="form-control @error('jenis_kriteria')
+                            is-invalid
+                        @enderror" id="jenis_kriteria" name="jenis_kriteria" aria-label="Default select example">
+                    <option value="" hidden>Select jenis_kriteria</option>
+                    <option value="Benefit" @selected(old('jenis_kriteria', $kriteria->jenis_kriteria == 'Benefit' >Benefit</option>
+                    <option value="Cost" @selected(old('jenis_kriteria', $kriteria->jenis_kriteria == 'Cost' >Cost</option>
+                </select>
+                @error('jenis_kriteria')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label class="col-sm-2 col-form-label" for=""></label>
+            <div class="col-sm-10">
+                <button type="submit" class="btn btn-primary">Ubah</button>
             </div>
         </div>
     </form>
