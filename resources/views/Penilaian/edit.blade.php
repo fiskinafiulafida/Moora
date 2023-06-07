b5975ad888763b7dbcc729352c46ec6837746eaf
fiskinafiulafida <fiskinafiul@gmail.com>
2023-06-07 20:09:13 +0700

    Edit Penilaian

--- resources/views/Penilaian/edit.blade.php
+++ resources/views/Penilaian/edit.blade.php
@@ -117,16 +117,16 @@
         @csrf
         <input type="hidden" class="form-control" id="alternatif_id" name="alternatif_id" required value="{{ $penilaian->alternatif->id }}" />
         @foreach ($kriteria as $kr)
+        <label class="col-sm-2 col-form-label" for="{{ $kr->nama_kriteria }}">{{ $kr->nama_kriteria }}</label>
         <div class="row mb-3">
-            <label class="col-sm-2 col-form-label" for="{{ $kr->nama_kriteria }}">{{ $kr->nama_kriteria }}</label>
             <div class="col-sm-10">
-                <select id="{{ $kr->nama_kriteria }}" class="form-select @error('sub_kriteria{{ $loop->iteration }}_id')
+                <select id="{{ $kr->nama_kriteria }}" class="form-control @error('sub_kriteria{{ $loop->iteration }}_id')
                                 is-invalid
                             @enderror" name="sub_kriteria{{ $loop->iteration }}_id">
-                    <option value="" hidden>Select {{ $kr->nama_kriteria }}</option>
+                    <!-- <option value="" hidden>Select {{ $kr->nama_kriteria }}</option> -->
                     @foreach ($subkriteria as $sub)
                     @if ($sub->kriteria_id == $kr->id)
-                    <option value="{{$sub->id}}" @selected(old('{{ $kr->id }}', $kr->id) == '{{$sub->id}}')>{{ $sub->nama_sub }}</option>
+                    <option value="{{$sub->id}}" @selected(old('{{ $kr->id }}', $kr->id == '{{$sub->id}}' > {{ $sub->nama_sub }}</option>
                     @endif
                     @endforeach
                 </select>
